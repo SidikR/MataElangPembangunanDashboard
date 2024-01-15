ae006521051b26da1c5cb56e209236fc26ff423d
SidikR <rahmad.119140096@student.itera.ac.id>
2024-01-15 22:09:36 +0700

    update header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,11 +7,8 @@
     <title>Dashboard - Mazer Admin Dashboard</title>
 
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
-    {{-- <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"> --}}
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
-    {{-- <link rel="stylesheet" href="{{ asset('assets/compiled/css/table-datatable-jquery.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('assets/extensions/datatables.net-bs5/css/dataTables.bootstrap5.min.css') }}">
-    {{-- <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}"> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.css">
     <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
@@ -21,39 +18,106 @@
 
 </head>
 
-<body>
-    <div id="app">
+<body style="overflow: hidden">
+    <div id="app" class="app-wrapper wkwk">
+        <div class="d-flex flex-column row">
 
-        @include('partials.sidebar')
+            @include('partials.header')
 
-        <div id="main">
-            <header class="mb-3">
-                <a href="#" class="burger-btn d-block d-xl-none">
-                    <i class="bi bi-justify fs-3"></i>
-                </a>
-            </header>
+            <div class="col">
+                <div class="main row z-0">
 
-            <div class="page-heading">
-                <h3>{{ $data['header_name'] ?? 'Dashboard' }}</h3>
+                    @include('partials.sidebar')
+
+                    <div id="main" class="col m-2 me-4" style="overflow-x: auto; height: 90vh; width: auto">
+
+                        <div class="page-heading">
+                            <h3>{{ $data['header_name'] ?? 'Dashboard' }}</h3>
+                        </div>
+
+                        <div class="page-content">
+                            @yield('content')
+                        </div>
+
+                        @include('partials.footer')
+
+                    </div>
+                </div>
             </div>
-
-            <div class="page-content">
-                @yield('content')
-            </div>
-
-            @include('partials.footer')
-
         </div>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const sidebarToggle = document.getElementById('sidebarToggle');
+            const sidebar = document.getElementById('sidebar');
+            const iconToggle = document.getElementById('iconToggle');
+            const toggleBtn = document.getElementById('toggle-btn');
+            const toggleBtnAside = document.getElementById('iconToggledAside');
+            var isSidebarOpen = true;
+
+            sidebarToggle.addEventListener('click', function() {
+                if (isSidebarOpen) {
+                    sidebar.style.display = 'none';
+                    iconToggle.classList.add('bi-list', 'fs-5', 'fw-bold');
+                    iconToggle.classList.remove('bi-x', 'fs-5', 'fw-bold');
+                    toggleBtnAside.classList.add('bi-caret-right');
+                    toggleBtnAside.classList.remove('bi-caret-left');
+                    toggleBtn.style.position = 'fixed';
+                    toggleBtn.style.top = '50%';
+                    toggleBtn.style.left = '0px';
+                } else {
+                    sidebar.style.display = 'block';
+                    iconToggle.classList.remove('bi-list', 'fs-5', 'fw-bold');
+                    iconToggle.classList.add('bi-x', 'fs-5', 'fw-bold');
+                    toggleBtnAside.classList.remove('bi-caret-right');
+                    toggleBtnAside.classList.add('bi-caret-left');
+                }
+
+                isSidebarOpen = !isSidebarOpen;
+                if (isSidebarOpen) {
+                    toggleBtn.style.removeProperty('position');
+                    toggleBtn.style.removeProperty('top');
+                    toggleBtn.style.removeProperty('left');
+                }
+            });
+
+            toggleBtn.addEventListener('click', function() {
+                if (isSidebarOpen) {
+                    sidebar.style.display = 'none';
+                    iconToggle.classList.add('bi-list', 'fs-5', 'fw-bold');
+                    iconToggle.classList.remove('bi-x', 'fs-5', 'fw-bold');
+                    toggleBtn.style.position = 'fixed';
+                    toggleBtn.style.top = '50%';
+                    toggleBtn.style.left = '0px';
+                    toggleBtnAside.classList.add('bi-caret-right');
+                    toggleBtnAside.classList.remove('bi-caret-left');
+                } else {
+                    sidebar.style.display = 'block';
+                    iconToggle.classList.remove('bi-list', 'fs-5', 'fw-bold');
+                    iconToggle.classList.add('bi-x', 'fs-5', 'fw-bold');
+                    toggleBtnAside.classList.remove('bi-caret-right');
+                    toggleBtnAside.classList.add('bi-caret-left');
+                }
+                isSidebarOpen = !isSidebarOpen;
+
+                if (isSidebarOpen) {
+                    toggleBtn.style.removeProperty('position');
+                    toggleBtn.style.removeProperty('top');
+                    toggleBtn.style.removeProperty('left');
+                }
+
+            });
+
+        });
+    </script>
+
     <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/js/app.js') }}"></script>
 
-    <!-- Need: Apexcharts -->
     <script src="{{ asset('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ asset('assets/js/pages/dashboard.js') }}"></script>
-    {{-- <script src="{{ asset('assets/static/js/components/dark.js') }}"></script> --}}
     <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-    {{-- <script src="{{ asset('assets/compiled/js/app.js') }}"></script> --}}
     <script src="{{ asset('assets/extensions/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/extensions/datatables.net/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/extensions/datatables.net-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
