940df135e3fcc34b1092d43f4c85c5901a0a4eca
SidikR <rahmad.119140096@student.itera.ac.id>
2024-01-16 08:19:39 +0700

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                    href="https://saugi.me">Saugi</a></p>
+                    href="https://saugi.me">Pemerintah Kabupaten Lampung Selatan</a></p>
         </div>
     </div>
 </footer>
